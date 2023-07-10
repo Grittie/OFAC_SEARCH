c178116b16dc4f6d26eaaeaeeed2224053b612dd
Grittie <grittielars@gmail.com>
2023-07-11 00:34:05 +0200

    initial project file push

---
+++ OFAC-Search/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Linq;
+using System.Windows;
+
+namespace OFAC_Search
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(DirSrc.Text))
+            {
+                MessageBox.Show("Directory can't be empty");
+                return;
+            }
+            else if (!Directory.Exists(DirSrc.Text))
+            {
+                MessageBox.Show("Directory not found");
+                return;
+            }
+            StartBtn.IsEnabled = false;
+            OpenSource.IsEnabled = false;
+            resVal.Text = string.Empty;
+
+            var folders = Directory.GetDirectories(DirSrc.Text, "*", SearchOption.AllDirectories).ToList();
+            folders.Insert(0, DirSrc.Text);
+            int index = 0;
+
+            foreach (var folder in folders)
+            {
+                resVal.Text = $"{++index} / {folders.Count}";
+                var webView = new WebviewWindow(folder);
+                webView.init();
+                webView.ShowDialog();
+            }//
+
+            StartBtn.IsEnabled = true;
+            OpenSource.IsEnabled = true;
+        }
+
+        private void OpenSource_Click(object sender, RoutedEventArgs e)
+        {
+            using (var fbd = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                System.Windows.Forms.DialogResult result = fbd.ShowDialog();
+                if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    DirSrc.Text = fbd.SelectedPath;
+            }
+        }//
+    }
+}
---
+++ OFAC-Search/WebviewWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace OFAC_Search
+{
+    /// <summary>
+    /// Interaction logic for WebviewWindow.xaml
+    /// </summary>
+    public partial class WebviewWindow : Window
+    {
+        string Folder = string.Empty;
+        public WebviewWindow(string folder)
+        {
+            InitializeComponent();
+            Folder = folder;
+        }
+
+        public async void init()
+        {
+            await webView.EnsureCoreWebView2Async();
+            webView.Source = new Uri("https://sanctionssearch.ofac.treas.gov");
+            webView.CoreWebView2.DOMContentLoaded += CoreWebView2_DOMContentLoaded;
+            webView.CoreWebView2.WebMessageReceived += CoreWebView2_WebMessageReceived;
+        }//
+
+        private void CoreWebView2_WebMessageReceived(object? sender, CoreWebView2WebMessageReceivedEventArgs e)
+        {
+        }
+
+        private void CoreWebView2_DOMContentLoaded(object? sender, CoreWebView2DOMContentLoadedEventArgs e)
+        {
+            var dirName = new DirectoryInfo(Folder).Name;
+            webView.CoreWebView2.DOMContentLoaded -= CoreWebView2_DOMContentLoaded;
+            webView.CoreWebView2.DOMContentLoaded += CoreWebView2_DOMContentLoaded2;
+            var script = @"var compName = document.getElementById(""ctl00_MainContent_txtLastName"");" +
+                        $"compName.value= '{dirName}';" +
+                        @"var searchBtn = document.getElementById(""ctl00_MainContent_btnSearch"");
+                        searchBtn.click();";
+            webView.CoreWebView2.ExecuteScriptAsync(script);
+        }//
+
+        private async void CoreWebView2_DOMContentLoaded2(object? sender, CoreWebView2DOMContentLoadedEventArgs e)
+        {
+            var printSettings = webView.CoreWebView2.Environment.CreatePrintSettings();
+
+            await webView.CoreWebView2.PrintToPdfAsync(Path.Combine(Folder, $"OFAC {DateTime.Now.ToString("yyyy-MM-dd")}.pdf"), printSettings);
+            this.Close();
+        }
+    }
+}
